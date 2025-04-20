beba42c0207262b68415cfefda784c5a3a06c5cd
Rashika <rashika.karmacharya@gmail.com>
2025-04-20 01:11:54 -0500

    Initial commit with MAUI build

---
+++ CSC317_Tracker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace CSC317_Tracker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ CSC317_Tracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CSC317_Tracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
